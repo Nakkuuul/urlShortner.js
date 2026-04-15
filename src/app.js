efafbdf1c2c35439ab247c24e6b2a44cad431b1f
sapphirebroking <nakul.thakur@sapphirebroking.com>
2026-04-15 22:16:08 +0530

    feat: add cors support

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 
 import authRouter from "./routes/auth.routes.js";
 import urlRouter from "./routes/url.routes.js";
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(cors());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/url", urlRouter);
