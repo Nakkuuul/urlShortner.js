f3531e0c1ab7175769dcdaf713b7f4a64dec3136
sapphirebroking <nakul.thakur@sapphirebroking.com>
2026-04-15 19:08:45 +0530

    feat: login controller and routes

--- src/config/env.js
+++ src/config/env.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const MONGOOSE_URI = process.env.MONGOOSE_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 if (!PORT) {
   throw new Error("PORT is not available in .env");
@@ -13,7 +14,12 @@ if (!MONGOOSE_URI) {
   throw new Error("MONGOOSE_URI is not available in .env");
 }
 
+if (!JWT_SECRET) {
+  throw new Error("JWT_SECRET is not available in .env");
+}
+
 export default {
   PORT,
   MONGOOSE_URI,
+  JWT_SECRET
 };
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import userModel from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import env from "../config/env.js";
 
 export async function register(req, res) {
   try {
@@ -55,6 +57,68 @@ export async function register(req, res) {
         username: createUser.username,
         email: createUser.email,
       },
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
+
+export async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    if (!email) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is required",
+      });
+    }
+
+    if (!password) {
+      return res.status(400).json({
+        success: false,
+        message: "Password is required",
+      });
+    }
+
+    const normalisedEmail = email.toLowerCase().trim();
+
+    const user = await userModel.findOne({
+      email: normalisedEmail,
+    });
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    const token = jwt.sign({ id: user._id }, env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged-in successfully",
+      data: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+      },
+      token: token,
     });
   } catch (error) {
     return res.status(500).json({
